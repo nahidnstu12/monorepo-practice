e04ab83d67b8e4e8736682ce76683d326254744a
nahidnstu12 <nahid.dev19@gmail.com>
2023-02-08 17:56:41 +0600

    frontend connect

--- apps/api/src/app/helper/db.helper.ts
+++ apps/api/src/app/helper/db.helper.ts
@@ -2,6 +2,8 @@ import * as mongoose from 'mongoose';
 
 export class DBHelper {
     static init(): void {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    //    @ts-ignore
         mongoose.connect('mongodb://localhost/todoDB', {useNewUrlParser: true, useUnifiedTopology: true})
             .then(() => console.log('Connection to mongoDB successful'))
             .catch((e: Error) => console.log(`Could not connect to mongo.\n\n${e}`));
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,21 +1,37 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
+import express = require('express');
+import * as bodyParser from 'body-parser';
+import  cors = require('cors');
 
-import express from 'express';
-import * as path from 'path';
+import { DBHelper } from './app/helper/db.helper';
+import { TodoService } from './app/todo/todo.service';
 
 const app = express();
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(cors())
+
+app.use(bodyParser.json());
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to api!' });
 });
 
-const port = process.env.PORT || 3333;
+app.get('/api/todos', async (req, res) => {
+  res.json(await TodoService.getAll());
+});
+
+app.post('/api/todos', async (req, res) => {
+  res.json(await TodoService.create(req.body));
+});
+
+app.delete('/api/todos/:id', async (req, res) => {
+  res.json(await TodoService.delete(req.params.id));
+});
+
+const port = process.env.port || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
+
+DBHelper.init();
